e6da8b4cc7c49d206224fdb30ec70842eea2a7a1
betchieph <betchieph>
2020-08-19 21:30:33 +0000

    first commit

---
+++ src/components/bbq.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import bbq from './images/bbq.jpg';
+import './css/slice.css';
+
+function Bbq() {
+  return(
+    <div className="Bbq col-4">
+      <div className="card">
+        <img src={ bbq } class="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">BBQ Pizza</h5>
+          <p className="card-text">Perhaps the most popular price we offer, BBQ Chicken over a delicious dollar pizza. What more could one want in life?</p>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Price: $1.00</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Bbq;
+
+  
